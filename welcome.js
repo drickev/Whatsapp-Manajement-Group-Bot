f674cc0c6e7fa6c81d8f781d6719007d281d08ee
dvc <apriantohaerul@gmail.com>
2025-06-29 23:54:25 -0700

    Create welcome.js

---
+++ welcome.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+
+module.exports = async function welcomeHandler(sock, update) {
+  if (update.action === "add") {
+    try {
+      const metadata = await sock.groupMetadata(update.id);
+      for (const participant of update.participants) {
+        const name = participant.split("@")[0];
+        const caption = 
+`custom message @${name}
+your message here;
+
+        await sock.sendMessage(update.id, {
+          image: fs.readFileSync("./banner.jpg"), // Bisa juga .jpg atau .png
+          caption: caption,
+          mentions: [participant]
+        });
+      }
+    } catch (err) {
+      console.error("❌ Error kirim pesan sambutan:", err);
+    }
+  }
+};
